c1d9b8ee5915bd4c68da3281721dbf16e4442aec
Alex Lamm <alexlammseven@gmail.com>
2017-06-08 16:09:13 -0400

    all

---
+++ main.js
@@ -0,0 +1,14 @@
+function formValidation()
+{
+
+var firstname = document.registration.firstname;
+var lastname = document.registration.lastname;
+var email = document.registration.email;
+var zip = document.registration.zip;
+var url = document.registration.url;
+var lang = document.registration.lang;
+var comm = document.registration.comm;
+var mobile = document.registration.mobile;
+var home = document.registration.home;
+
+}
